1444d5b48db565c2f6052f7a1ed7c383c414afb0
ubairnoor <ubairnoor5@gmail.com>
2022-02-02 15:27:12 +0530

    Nested block Issue Fixed

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,18 +1,13 @@
 const useGenres = (selectedGenres) => {
-    {/* if the selected genre is 0 then return nothing*/
-    }
+    /* if the selected genre is 0 then return nothing*/
     if (selectedGenres.length < 1) return "";
-    {/* with the map function we get all ids in selected genres*/
-    }
-
+    /* with the map function we get all ids in selected genres*/
     const GenreIds = selectedGenres.map ( ( g ) => g.id );
-    {/* after getting ids we will use reduce it. ist value is accumulator and
+    /* after getting ids we will use reduce it. ist value is accumulator and
     second  is current value.
     so in return we will  accumulator + comma + current value.
     if we have  1,2,3,4 after return it will look like this [1,2,3,4]
     */
-    }
-
     return GenreIds.reduce ( ( acc , curr ) => acc + ',' + curr );
 }
 export default useGenres;

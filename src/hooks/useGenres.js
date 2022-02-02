fe352e46acc3f9ef17147c7c36f3f34814f1a628
ubairnoor <ubairnoor5@gmail.com>
2022-02-02 15:07:10 +0530

    Add comments in use Genres hooks

--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,6 +1,18 @@
 const useGenres = (selectedGenres) => {
+    {/* if the selected genre is 0 then return nothing*/
+    }
     if (selectedGenres.length < 1) return "";
-    const GenreIds = selectedGenres.map((g) => g.id);
-    return GenreIds.reduce((acc, curr) => acc + ',' + curr);
+    {/* with the map function we get all ids in selected genres*/
+    }
+
+    const GenreIds = selectedGenres.map ( ( g ) => g.id );
+    {/* after getting ids we will use reduce it. ist value is accumulator and
+    second  is current value.
+    so in return we will  accumulator + comma + current value.
+    if we have  1,2,3,4 after return it will look like this [1,2,3,4]
+    */
+    }
+
+    return GenreIds.reduce ( ( acc , curr ) => acc + ',' + curr );
 }
 export default useGenres;
